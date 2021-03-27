0eb7a02914a3ab4fa879215fa9648df7e004e21a
wjdqhry <yung12320@daum.net>
2021-03-27 13:17:58 +0900

    add archiver

--- main.go
+++ main.go
@@ -3,12 +3,47 @@ package main
 import (
 	"compress/flate"
 	"log"
+	"sync"
 
 	"github.com/mholt/archiver"
 )
 
 func main() {
-	zip := archiver.Zip{
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(3)
+	go func() {
+		newArchive(createZipArchiver(), "/Users/jungbokyo/go/src/fileCompression/bogus", "/Users/jungbokyo/go/src/fileCompression/bokyo.zip")
+		wg.Done()
+		log.Println("1 done")
+	}()
+
+	go func() {
+		newArchive(createZipArchiver(), "/Users/jungbokyo/go/src/fileCompression/boguscopy2", "/Users/jungbokyo/go/src/fileCompression/bokyo_copy_2.zip")
+		wg.Done()
+		log.Println("2 done")
+	}()
+	go func() {
+		newArchive(createZipArchiver(), "/Users/jungbokyo/go/src/fileCompression/boguscopy3", "/Users/jungbokyo/go/src/fileCompression/bokyo_copy_3.zip")
+
+		wg.Done()
+		log.Println("3 done")
+	}()
+	wg.Wait()
+
+	log.Println("all done")
+}
+
+func newArchive(zip archiver.Zip, target string, dest string) {
+	err := zip.Archive([]string{target}, dest)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createZipArchiver() archiver.Zip {
+	return archiver.Zip{
 		CompressionLevel:       flate.BestCompression,
 		MkdirAll:               true,
 		SelectiveCompression:   true,
@@ -16,9 +51,4 @@ func main() {
 		OverwriteExisting:      true,
 		ImplicitTopLevelFolder: true,
 	}
-	err := zip.Archive([]string{"bogus"}, "./bokyo.zip")
-	if err != nil {
-		log.Fatal(err)
-	}
-	//err = archiver.Extract()
 }
